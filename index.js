a0177a289c7c78383ed422368477d74db9a2d7ec
Gaurav Rattan <gauravrattan76@gmail.com>
2020-03-20 08:53:38 +0530

    react redux with reducer

--- index.js
+++ index.js
@@ -1,10 +1,30 @@
-const BUY_CAKE ="BUY_CAKE"
+//Action iS a plain javsacripot object with type property and we use const to m ake sure that there are no type rrrors
 
+const BUY_CAKE = 'Buy_Cake'
+
+
+//Action Creator is a function whos task is to return a action
 
 function buyCake(){
-    return{
+    return {
         type:BUY_CAKE,
-        info:'First Redux action'  
+        info:'First Action created!!'
     }
 }
 
+//application state needs to be  a single object
+
+const initialState = {
+    numOfCakes:10
+}
+
+const reducer = (state= initialState,action) => {
+    switch(action.type){
+        case BUY_CAKE : return {
+            ...state,
+            numOfCakes : state.numOfCakes -1
+        }
+        default: return state
+    }
+
+}
