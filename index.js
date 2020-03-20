5db1f8fdc73147426bed06cb9f41a1a62a9ab9c8
Gaurav Rattan <gauravrattan76@gmail.com>
2020-03-20 09:17:21 +0530

    store checkin

--- index.js
+++ index.js
@@ -1,3 +1,6 @@
+const redux = require('redux');
+const createStore = redux.createStore;
+
 //Action iS a plain javsacripot object with type property and we use const to m ake sure that there are no type rrrors
 
 const BUY_CAKE = 'Buy_Cake'
@@ -18,6 +21,7 @@ const initialState = {
     numOfCakes:10
 }
 
+//reducer is a function that takes the current state and action and return the new state
 const reducer = (state= initialState,action) => {
     switch(action.type){
         case BUY_CAKE : return {
@@ -28,3 +32,14 @@ const reducer = (state= initialState,action) => {
     }
 
 }
+//Creating a store using redux library create store method and pasing the initialk state using reducer
+const store = createStore(reducer);
+//store.getstate method to get the current state
+console.log('initial state',store.getState());
+//subscribing to the store
+const unsubscribe = store.subscribe(() => console.log('new state',store.getState()));
+//dispatching the actions using action creator
+store.dispatch(buyCake());
+store.dispatch(buyCake());
+store.dispatch(buyCake());
+unsubscribe();
